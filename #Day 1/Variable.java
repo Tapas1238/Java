d4ab40f6c2d865b12b9854e7d46af3fe1f644032
Tapas1238 <71552539+Tapas1238@users.noreply.github.com>
2022-07-25 19:29:19 +0530

    Create Variable.java

---
+++ #Day 1/Variable.java
@@ -0,0 +1,25 @@
+package java_basics;
+
+class  Variable{
+    public static void main(String[] args){
+        int x=-4;
+        System.out.format("printing int value: %d \n",x);
+        char c ='h';
+        System.out.format("Printing the char value: %c\n",c);
+        String s="String variable..";
+        System.out.format("Printing the string value: %s\n",s);
+        double d=93.9;
+        System.out.format("Printing the double value: %f\n",d);
+        short srt=32767;
+        System.out.format("Printing the sort value: %d\n",srt);
+        long l=2894793734934L;
+        System.out.format("Printing the long value: %d\n",l);
+        byte by=127;
+        System.out.format("Printing the byte value: %d\n",by);
+        boolean b=true;
+        System.out.format("Printing the boolean value: %b\n",b);
+        float f=19.3f;
+        System.out.format("Printing the float value: %f\n",f);
+        
+    }
+}
